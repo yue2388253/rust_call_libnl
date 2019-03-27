9d19e942a33ae4357add962dd13ba03ac637bbcf
graceon <graceon@163.com>
2019-03-27 07:15:38 +0000

    add_qdsic_tbf delete_qdsic

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,35 @@ use std::io;
 use std::process::Command;
 
 
+use std::ffi::CString;
+use std::os::raw::c_char;
+
+
 // Bytes in second
-unsafe fn call_set_rate(rate :u32, bucket :u32){
+unsafe fn call_add_qdisc_tbf(if_name : *const c_char,maj :u32, min :u32,limit :u32 ,rate :u32, bucket :u32)->i32{
     let rate = rate * 1000 / 8;
-    set_rate ( rate as i32,
-               bucket as i32);
+    let bucket = bucket;
+    add_qdisc_tbf(if_name,maj as i32,min as i32,limit as i32, rate as i32,
+               bucket as i32)
 }
 
-
+unsafe fn call_delete_qdisc(if_name : *const c_char,maj :u32, min :u32)->i32{
+    delete_qdisc(if_name, maj as i32,
+               min as i32)
+}
 fn main() {
+	let if_name=CString::new("ens33").unwrap();
+    let if_name_ptr=if_name.as_ptr();
     loop {
         println!("Please input the rate(kbits per second): ");
         let mut rate = String::new();
         io::stdin().read_line(&mut rate)
             .expect("Failed to read line.");
         let rate = match rate.trim().as_ref() {
-            "" => break,
+            "" => unsafe{
+                	println!("->info delete_qdisc:err->{}",call_delete_qdisc(if_name_ptr,1,0));
+                	break;
+            	}
             rate => {
                 rate.parse::<u32>()
                     .expect("Please input a positive integer.")
@@ -36,9 +49,29 @@ fn main() {
                     .expect("Please input a positive integer.")
             }
         };
+        let mut queue_limit = String::new();
+        println!("Please input the queue_limit(bytes): ");
+        io::stdin().read_line(&mut queue_limit)
+            .expect("Failed to read line.");
+        let queue_limit = match queue_limit.trim().as_ref() {
+            "" => break,
+            queue_limit => {
+                queue_limit.parse::<u32>()
+                    .expect("Please input a positive integer.")
+            }
+        };
+
+
+        //https://linux.die.net/man/8/tc-tbf
         unsafe {
-            call_set_rate(rate, bucket);
-        }
+        	println!("->info add_qdisc_tbf:err->{}",call_add_qdisc_tbf(if_name_ptr,1,0,queue_limit,rate, bucket));
+    	};
+
+
+
+
+
+
         let qdisc_output = Command::new("tc")
             .arg("qdisc")
             .arg("show")
--- src/rate_control.c
+++ src/rate_control.c
@@ -1,69 +1,129 @@
 //
 // Created by lam on 19-3-23.
 //
-
 #include "rate_control.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <arpa/inet.h> //inet_ntop
 #include <netlink/netlink.h>
-#include <netlink/genl/genl.h>
-#include <netlink/netlink.h>
 #include <netlink/utils.h>
+#include <netlink/genl/genl.h>
 #include <netlink/route/rtnl.h>
 #include <netlink/route/link.h>
 #include <netlink/route/tc.h>
 #include <netlink/route/qdisc.h>
 #include <netlink/route/qdisc/tbf.h>
-
 /*
  * rate: New rate in bytes per second.
  * bucket: Size of bucket in bytes.
  */
-int set_rate(int rate, int bucket) {
+int add_qdisc_tbf(char const * if_name,int maj,int min,int limit,int rate, int bucket) 
+{
     struct nl_sock *sock;
     struct rtnl_qdisc *q;
     struct nl_cache *cache;
     struct rtnl_link *link;
     int if_index;
-    int maj = 1;
-    int min = 0;
-
     sock = nl_socket_alloc();
-    if (nl_connect(sock, NETLINK_ROUTE) < 0) {
+    if(sock==NULL){
         return -1;
     }
-
-    if (rtnl_link_alloc_cache(sock, AF_UNSPEC, &cache) < 0) {
+    if (nl_connect(sock, NETLINK_ROUTE) < 0) 
+    {
         return -2;
     }
-
-    link = rtnl_link_get_by_name(cache, "ens33");
+    if (rtnl_link_alloc_cache(sock, AF_UNSPEC, &cache) < 0) 
+    {
+        return -3;
+    }
+    link = rtnl_link_get_by_name(cache, if_name);
+    nl_cache_free(cache);
+    if( link==NULL) 
+    {
+        return -4;
+    }
     if_index = rtnl_link_get_ifindex(link);
-
+    if( if_index==0) 
+    {
+        return -5;
+    }
     q = rtnl_qdisc_alloc();
+    if( q ==NULL){
+        return -6;
+    }
+    rtnl_tc_set_ifindex(TC_CAST(q), if_index);
+    rtnl_tc_set_parent(TC_CAST(q), TC_H_ROOT);
+    rtnl_tc_set_handle(TC_CAST(q), TC_HANDLE(maj,min));
+    if(rtnl_tc_set_kind(TC_CAST(q), "tbf")<0){
+        return -7;
+    };
+
+    //https://linux.die.net/man/8/tc-tbf
+    rtnl_qdisc_tbf_set_limit(q, limit);
+    rtnl_qdisc_tbf_set_rate(q, rate, bucket, 0);
+/*
+netem okay, htb okay, please comment 
+and uncomment the special parameters for the qdiscs
+rtnl_netem_set_delay(q, 100);
+rtnl_netem_set_loss(q, 10);
+*/
+
+    if(rtnl_qdisc_add(sock, q, NLM_F_CREATE)<0){
+        return -10;
+    };
+    rtnl_qdisc_put(q);
+    rtnl_link_put(link);
+    
+    nl_socket_free(sock);
+    return 0;
+}
+
+
+int delete_qdisc(char const * if_name,int maj, int min){
+    struct nl_sock *sock;
+    struct rtnl_qdisc *q;
+    struct nl_cache *cache;
+    struct rtnl_link *link;
+    int if_index;
+    sock = nl_socket_alloc();
+    
+    if(sock==NULL){
+        return -1;
+    }
+    if (nl_connect(sock, NETLINK_ROUTE) < 0) 
+    {
+        return -2;
+    }
+    if (rtnl_link_alloc_cache(sock, AF_UNSPEC, &cache) < 0) 
+    {
+        return -3;
+    }
+    link = rtnl_link_get_by_name(cache, if_name);
+    if( link==NULL) 
+    {
+        return -4;
+    }
+    nl_cache_free(cache);
+    if_index = rtnl_link_get_ifindex(link);
+    if( if_index==0) 
+    {
+        return -5;
+    }
+    q = rtnl_qdisc_alloc();
+    if( q ==NULL){
+        return -6;
+    }
 
     rtnl_tc_set_ifindex(TC_CAST(q), if_index);
     rtnl_tc_set_parent(TC_CAST(q), TC_H_ROOT);
-    rtnl_tc_set_handle(TC_CAST(q), TC_HANDLE(maj, min));
-    rtnl_tc_set_kind(TC_CAST(q), "tbf");
+    rtnl_tc_set_handle(TC_CAST(q), TC_HANDLE(maj,min));
 
-    /*
-     * netem okay, htb okay, please comment
-     * and uncomment the special parameters for the qdiscs
-     */
-    rtnl_qdisc_tbf_set_limit(q, rate);
-    rtnl_qdisc_tbf_set_rate(q, rate, bucket, 0);
-    /*
-     * rtnl_netem_set_delay(q, 100);
-     * rtnl_netem_set_loss(q, 10);
-     */
-
-    rtnl_qdisc_add(sock, q, NLM_F_CREATE);
-
+    if(rtnl_qdisc_delete(sock, q)<0){
+        return -10;
+    };
     rtnl_qdisc_put(q);
-    nl_socket_free(sock);
     rtnl_link_put(link);
-    nl_cache_put(cache);
+
+    nl_socket_free(sock);
     return 0;
 }
